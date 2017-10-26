f361795b47e0759028b57bbc493b24ec512cc606
Sneh Pandya <sneh.pandya1@gmail.com>
2017-10-26 09:53:30 +0530

    Espresso: Test for RecyclerView

--- app/src/main/java/com/snehpandya/androidtesting/Names.java
+++ app/src/main/java/com/snehpandya/androidtesting/Names.java
@@ -18,39 +18,14 @@ public class Names {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public static ArrayList<Names> createList() {
         ArrayList<Names> names = new ArrayList<>();
 
-        names.add(new Names("A"));
-        names.add(new Names("B"));
-        names.add(new Names("C"));
-        names.add(new Names("D"));
-        names.add(new Names("E"));
-        names.add(new Names("F"));
-        names.add(new Names("G"));
-        names.add(new Names("H"));
-        names.add(new Names("I"));
-        names.add(new Names("J"));
-        names.add(new Names("K"));
-        names.add(new Names("L"));
-        names.add(new Names("M"));
-        names.add(new Names("N"));
-        names.add(new Names("O"));
-        names.add(new Names("P"));
-        names.add(new Names("Q"));
-        names.add(new Names("R"));
-        names.add(new Names("S"));
-        names.add(new Names("T"));
-        names.add(new Names("U"));
-        names.add(new Names("V"));
-        names.add(new Names("W"));
-        names.add(new Names("X"));
-        names.add(new Names("Y"));
-        names.add(new Names("Z"));
+        for (char alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+            names.add(new Names(String.valueOf(alphabets)));
+
+        for (char alphabets = 'a'; alphabets <= 'z'; alphabets++)
+            names.add(new Names(String.valueOf(alphabets)));
 
         return names;
     }
